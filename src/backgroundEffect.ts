bcadbe1e882cc656fbc2d452240291fa3c36826f
Artem Basov <artem.basov@sharekey.com>
2024-12-27 21:36:00 +0300

    fix: background effect constraints type usage

--- src/backgroundEffect.ts
+++ src/backgroundEffect.ts
@@ -2,13 +2,13 @@ import { NativeModules } from 'react-native';
 
 const { WebRTCModule } = NativeModules;
 
-export interface Constraints {
+export interface BackgroundEffectConstraints {
     enableBlurBackgroud?: boolean;
     backgroundImageBase64?: string | null;
     enableVirtualBackgroud?: boolean;
 }
 
-export default function changeBackgroundEffect(constraints: Constraints = {}): Promise<void> {
+export default function changeBackgroundEffect(constraints: BackgroundEffectConstraints = {}): Promise<void> {
     return new Promise((resolve, reject) => {
         return WebRTCModule.changeBackgroundEffect(constraints, (data) => {                
             resolve(data);
